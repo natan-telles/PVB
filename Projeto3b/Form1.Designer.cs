38aac8c3aef631933f11eb4df5c7241e2a2ad6f8
Natan Telles <natanstelles@gmail.com>
2024-09-04 17:00:25 -0300

    projeto_pagamento_de_parcelas

---
+++ Projeto3b/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Projeto3b
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            label5 = new System.Windows.Forms.Label();
+            textBox5 = new System.Windows.Forms.TextBox();
+            label6 = new System.Windows.Forms.Label();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            label7 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox1.Location = new System.Drawing.Point(43, 72);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(205, 32);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox2.Location = new System.Drawing.Point(43, 163);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(205, 29);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox3.Location = new System.Drawing.Point(43, 259);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(205, 29);
+            textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(43, 28);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(169, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Valor da compra:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(43, 119);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(205, 30);
+            label2.TabIndex = 4;
+            label2.Text = "Numero de parcelas:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(43, 214);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(167, 30);
+            label3.TabIndex = 5;
+            label3.Text = "Data da compra:";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox4.Location = new System.Drawing.Point(654, 28);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            textBox4.Size = new System.Drawing.Size(257, 301);
+            textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(654, 350);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(114, 30);
+            label4.TabIndex = 7;
+            label4.Text = "Valor Total:";
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(42, 314);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(206, 36);
+            button1.TabIndex = 8;
+            button1.Text = "Enviar dados da compra";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label5.Location = new System.Drawing.Point(4, 71);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(37, 30);
+            label5.TabIndex = 9;
+            label5.Text = "R$";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox5.Location = new System.Drawing.Point(43, 445);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(205, 29);
+            textBox5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label6.Location = new System.Drawing.Point(42, 403);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(204, 30);
+            label6.TabIndex = 11;
+            label6.Text = "Data do pagamento:";
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(43, 494);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(206, 36);
+            button2.TabIndex = 12;
+            button2.Text = "Efetuar pagamento";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(216, 221);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(75, 23);
+            button3.TabIndex = 13;
+            button3.Text = "Data atual";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label7.Location = new System.Drawing.Point(618, 495);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(0, 20);
+            label7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label8.Location = new System.Drawing.Point(618, 536);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(0, 20);
+            label8.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(943, 565);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ Projeto3b/Form1.cs
@@ -0,0 +1,166 @@
+﻿/* *******************************************************************
+* Colegio Técnico Antônio Teixeira Fernandes (Univap)
+* Curso Técnico em Informática - Data de Entrega: 10/09/2024
+* Autores do Projeto: Leonardo Martinelli de Oliveira Lima
+*                     Natan Soares Telles
+* Turma: 2H
+* Projeto bimestral 3
+* Observação: 
+* 
+* textBox1 - inserir o valor da compra
+* textBox2 - inserir a quantidade de parcelas
+* textBox3 - inserir data da compra
+* textBox4 - parcelas da compra
+* textBox5 - inserir data do pagamento da parcela
+* label1 - texto valor da compra
+* label2 - texto numero de parcelas
+* label3 - texto data da compra
+* label4 - texto valor total da compra
+* label5 - texto R$ para o textBox1
+* label6 - texto data de pagamento 
+* label7 - valor com juros da parcela atrasada
+* label8 - valor acumulado dos juros
+* button1 - calcular parcelas
+* button2 - realizar pagamento da parcela
+* button3 - obter data no momento do clique
+* 
+* ************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto3b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //variáveis globias
+        float valorTotal;
+        int numeroParcelas;
+        DateTime dataCompra;
+        float valorParcela;
+        int parcelas_pagas;
+        float juros_acumulados;
+
+
+/*----------------------------------------------------------------------------------------------------------------*/
+        private void button1_Click(object sender, EventArgs e) //botão para preencher as parcelas de acordo com entrada do usuário
+        {
+            float.TryParse(textBox1.Text, out valorTotal);
+            int.TryParse(textBox2.Text, out numeroParcelas);
+            DateTime.TryParse(textBox3.Text, out dataCompra);
+
+            valorParcela = valorTotal / numeroParcelas;
+            textBox4.Text = ""; //zera o textBox para futuras simulações
+            label7.Text = ""; //zera o label de juros para futuras simulações
+
+
+            //preenchendo textBox com as parcelas
+            for (int i = 1; i <= numeroParcelas; i++) //laço de acordo com o nº de parcelas
+            {
+                DateTime dataVencimento = dataCompra.AddMonths(i);
+
+                while (dataVencimento.DayOfWeek == DayOfWeek.Saturday || dataVencimento.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    dataVencimento = dataVencimento.AddDays(1); //desconsidera finais de semana
+                }
+
+                //exibição
+                string linha = i.ToString() + ". " + dataVencimento.ToString("dd/MM/yyyy") + " - Valor: " + valorParcela.ToString("C2");
+                textBox4.AppendText(linha + Environment.NewLine);
+            }
+
+            label4.Text = "Valor Total: " + valorTotal.ToString("C2"); //atualizar Valor Total
+            parcelas_pagas = 0; //seta como padrão que nenhuma parcela foi paga
+        }
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+        private void button2_Click(object sender, EventArgs e) //botao para pagar parcela
+        {           
+            DateTime dataDoPagamento; //variáveis
+            DateTime.TryParse(textBox5.Text, out dataDoPagamento);
+            DateTime dataParcela;
+
+            
+            //verificar se a data do pagamento é válida e pagar parcela
+
+            DateTime.TryParse(textBox3.Text, out dataParcela); //data inserida pelo usuario para pagar
+            dataParcela = dataParcela.AddMonths(parcelas_pagas + 1); //obtém parcela atual para ser paga
+            while ((int)dataParcela.DayOfWeek == 6 || (int)dataParcela.DayOfWeek == 0)
+            {
+                dataParcela = dataParcela.AddDays(1);
+            }
+
+            int.TryParse(textBox2.Text, out numeroParcelas);
+            if (parcelas_pagas >= numeroParcelas)
+            {
+                MessageBox.Show("Todas as parcelas foram pagas!");
+                return;
+            }
+            else
+            {
+                if (dataDoPagamento < dataParcela) //data inválida para pagar 
+                {
+                    MessageBox.Show("Data inválida! Não é possível pagar parcela antes da data da compra;");
+                }
+                else if (dataDoPagamento == dataParcela) //parcela paga na data correta
+                {
+                    parcelas_pagas++;
+                    MessageBox.Show("Pagamento efetuado!" + Environment.NewLine + "Parcelas pagas: " + parcelas_pagas.ToString() + "/" + numeroParcelas.ToString());
+                    //removendo do total
+                    valorTotal -= valorParcela;
+                    label4.Text = "Valor Total: " + valorTotal.ToString("C2");
+                    label7.Text = ""; //zera o label de aviso de parcela atrasada
+                }
+                else if (dataDoPagamento > dataParcela) //parcela paga com atraso
+                {
+                    parcelas_pagas++;
+                    float parcelaComJuros = valorParcela * 1.03F;
+                    valorTotal = (valorTotal - valorParcela + parcelaComJuros) - valorParcela;
+                    valorParcela = int.Parse(textBox1.Text) / int.Parse(textBox2.Text);
+                    juros_acumulados += (parcelaComJuros - valorParcela);
+
+                    MessageBox.Show("Pagamento realizado com atraso!" + Environment.NewLine + "Parcelas pagas: " + parcelas_pagas.ToString() + "/" + numeroParcelas.ToString());
+                    label4.Text = "Valor Total: " + valorTotal.ToString("C2");
+                    label7.Text = "OBS: Parcela paga com atraso. \nValor reajustado com juros: " + parcelaComJuros.ToString("C2");
+                    label8.Text = "Juros acumulados: " + juros_acumulados.ToString("C2");
+                }
+            }     
+
+            //remover do textBox4 a parcela paga
+            textBox4.Text = "";
+            for (int i = 1 + parcelas_pagas; i <= numeroParcelas; i++) //preenche novamente o textBox4, pulando as linhas das parcelas pagas
+            {
+                DateTime dataVencimento = dataCompra.AddMonths(i);
+
+                while (dataVencimento.DayOfWeek == DayOfWeek.Saturday || dataVencimento.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    dataVencimento = dataVencimento.AddDays(1); //mesma lógica dos finais de semana
+                }
+
+                //exibição
+                string linha = i.ToString() + ". " + dataVencimento.ToString("dd/MM/yyyy") + " - Valor: " + valorParcela.ToString("C2");
+                textBox4.AppendText(linha + Environment.NewLine);
+            }
+
+        }
+/*----------------------------------------------------------------------------------------------------------------*/
+        //botao que obtem data atual e passa para a data da compra
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = DateTime.Now.ToString("dd/MM/yy");
+        }
+    }
+}
